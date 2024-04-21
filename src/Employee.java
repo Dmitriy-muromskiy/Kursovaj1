af359d423338e108a35417184100fd27e5e116b3
79206 <serdukovd.a.murom@yandex.ru>
2024-04-21 17:30:32 +0300

    initial commit

---
+++ src/Employee.java
@@ -0,0 +1,62 @@
+import java.util.Objects;
+
+public class Employee {
+    private static int COUNTER = 1;
+    private final String fullName;
+    private int department;
+    private float salary;
+    private final int id;
+
+    public Employee(String fullName, int department, float salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = COUNTER++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && Float.compare(salary, employee.salary) == 0 && id == employee.id && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+}
